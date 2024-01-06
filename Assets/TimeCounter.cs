889a7279c428569ada17e6d34c5886d4c8f7b144
okiyama <okiyama1472@gmail.com>
2024-01-06 11:41:07 +0900

    ローカルでサンプルの動作確認してたもののPush

---
+++ Assets/TimeCounter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System;
+using UniRx;
+using UnityEngine;
+
+/// <summary>
+/// カウントダウンしてその時の値を通知する
+/// 3,2,1,0,(OnCompleted) といったイベントが飛ぶ
+/// </summary>
+public class TimeCounter : MonoBehaviour
+{
+    [SerializeField] private int TimeLeft = 3;
+
+    //タイマストリームの実体はこのSubject
+    private Subject<int> timerSubject = new Subject<int>();
+
+    public IObservable<int> OnTimeChanged
+    {
+        get { return timerSubject; }
+    } 
+
+    void Start()
+    {
+        StartCoroutine(TimerCoroutine());
+
+        //現在のカウントを表示
+        timerSubject.Subscribe(x => Debug.Log(x));
+    }
+
+    IEnumerator TimerCoroutine()
+    {
+        yield return null;
+
+        var time = TimeLeft;
+        while (time >= 0)
+        {
+            timerSubject.OnNext(time--);
+            yield return new WaitForSeconds(1);     
+        }
+        timerSubject.OnCompleted();
+    }
+}
